2abe94accfdb38cc04d8adeb141dfc19522fddeb
Windu Purnomo <wpwindupurnomo@gmail.com>
2019-01-29 06:46:40 +0700

    Merging

---
+++ app/src/main/java/id/zcode/android/nusago/BuatPin.java
@@ -0,0 +1,13 @@
+package com.example.arob.nusago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuatPin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_pin);
+    }
+}
